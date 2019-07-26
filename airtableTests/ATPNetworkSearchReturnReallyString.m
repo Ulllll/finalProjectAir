13933d3b7074134ba850e0f43946e38f744f0e9a
Ulllll <nooryes1@gmail.com>
2019-07-26 21:00:10 +0300

    + ссылка на Яндекс.Расписание

--- airtableTests/ATPNetworkSearchReturnReallyString.m
+++ airtableTests/ATPNetworkSearchReturnReallyString.m
@@ -1,18 +1,18 @@
 //
-//  airtableTests.m
+//  ATPNetworkSearchReturnReallyString.m
 //  airtableTests
 //
-//  Created by Анастасия Рябова on 15/07/2019.
+//  Created by Анастасия Рябова on 26/07/2019.
 //  Copyright © 2019 Анастасия Рябова. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
 
-@interface airtableTests : XCTestCase
+@interface ATPNetworkSearchReturnReallyString : XCTestCase
 
 @end
 
-@implementation airtableTests
+@implementation ATPNetworkSearchReturnReallyString
 
 - (void)setUp {
     // Put setup code here. This method is called before the invocation of each test method in the class.
